e6ab4cd72c170fdaa19a53481148647998e60f47
coryswainston <swainston.cory89@gmail.com>
2018-08-17 16:22:49 -0600

    Highlight words on touch

--- app/src/main/java/com/coryswainston/smart/dictionary/listeners/WordGrabber.java
+++ app/src/main/java/com/coryswainston/smart/dictionary/listeners/WordGrabber.java
@@ -1,6 +1,11 @@
 package com.coryswainston.smart.dictionary.listeners;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -10,39 +15,42 @@ import android.widget.TextView;
  */
 
 public class WordGrabber implements View.OnTouchListener {
-    private Callback callback;
 
-    private MotionEvent down;
+    private static final String TAG = "WordGrabber";
+
+    private Callback callback;
+    private Float downY;
 
     public WordGrabber(Callback callback) {
         this.callback = callback;
     }
 
-
-
     @Override
     @SuppressLint("ClickableViewAccessibility")
     public boolean onTouch(View v, MotionEvent event) {
+        int eventType = event.getActionMasked();
 
-        if (event.getActionMasked() == MotionEvent.ACTION_DOWN && down != null) {
-//            down = event;
-            return false;
+        switch (eventType) {
+            case MotionEvent.ACTION_DOWN:
+                downY = event.getY();
+                return false;
+            case MotionEvent.ACTION_UP:
+                float yDiff = Math.abs(downY - event.getY());
+                if (yDiff < 100) {
+                    TextView tv = (TextView)v;
+                    getWordAndExecuteCallback(tv, event);
+                }
+                downY = null;
+                return true;
         }
-        down = event;
-//        if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-//            MotionEvent temp = down;
-//            down = null;
-//            if (Math.abs(temp.getX() - temp.getX()) > 10 ||
-//                    Math.abs(temp.getY() - event.getY()) > 10) {
-//                return false;
-//            }
-//        }
+        return false;
+    }
 
-        TextView view = (TextView)v;
-        int offset = view.getOffsetForPosition(event.getX(), event.getY());
-        CharSequence text = view.getText();
+    private void getWordAndExecuteCallback(TextView tv, MotionEvent event) {
+        int offset = tv.getOffsetForPosition(event.getX(), event.getY());
+        final String text = tv.getText().toString();
         if (offset >= text.length()) {
-            return false;
+            return;
         }
 
         int endIndex   = offset;
@@ -53,10 +61,13 @@ public class WordGrabber implements View.OnTouchListener {
         for (int i = offset; i > 0 && Character.isLetterOrDigit(text.charAt(i - 1)); i--) {
             startIndex--;
         }
-
-        callback.callback(text.subSequence(startIndex, endIndex).toString().toLowerCase());
-
-        return false;
+        String word = text.subSequence(startIndex, endIndex).toString().toLowerCase();
+        SpannableString ss = new SpannableString(text);
+        ss.setSpan(new BackgroundColorSpan(Color.WHITE), startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new ForegroundColorSpan(Color.rgb(40, 150, 255)),
+                startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setText(ss);
+        callback.callback(word);
     }
 
     public interface Callback {
